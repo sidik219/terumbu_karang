3ab865693433ba176e9175a3c03f03bf6d85f6a1
Feisal Abdurrahman <48938403+FeisalAR@users.noreply.github.com>
2020-11-20 22:38:17 +0700

    fixes row colour bug

--- controllers/TKController.php
+++ controllers/TKController.php
@@ -134,51 +134,11 @@ $isLoggedIn = isset($_SESSION['id_user']) && !empty($_SESSION['id_user']);
     
 
     function editWilayah(){
-        if (isset($_POST['submit'])) {
-            if ($_POST['submit'] == 'Simpan') {
-                $id_wilayah          = $_POST['id_wilayah'];
-                $nama_wilayah          = $_POST['tbnama_wilayah'];
-                $deskripsi_wilayah     = $_POST['txtdeskripsi_wilayah'];
-                $randomstring = substr(md5(rand()), 0, 7);
-
-                //Image upload
-                if (isset($_FILES['image_uploads'])) {
-                //Image upload
-                $target_dir  = "images/foto_wilayah/";
-                $foto_wilayah = $target_dir .'WIL_'.$randomstring. '.jpg';
-                move_uploaded_file($_FILES["image_uploads"]["tmp_name"], $foto_wilayah);  
-
-                }
-                else if($_FILES["file"]["error"] == 4) {
-                    $foto_wilayah = "images/fwdefault.png";
-                }
-
-                //---image upload end
-
-                $sqleditwilayah = "UPDATE t_wilayah
-                            SET nama_wilayah = :nama_wilayah, deskripsi_wilayah = :deskripsi_wilayah, foto_wilayah = :foto_wilayah
-                            WHERE id_wilayah = :id_wilayah";
-
-                $stmt = $pdo->prepare($sqleditwilayah);
-                $stmt->execute(['nama_wilayah' => $nama_wilayah, 'deskripsi_wilayah' => $deskripsi_wilayah, 'foto_wilayah' => $foto_wilayah, 'id_wilayah' => $id_wilayah]);
-
-                $affectedrows = $stmt->rowCount();
-                if ($affectedrows == '0') {
-                //echo "Update sukses";
-                } else {
-                header("Location: edit_wilayah.php?id_wilayah=$id_wilayah&status=updatesuccess");
-                }
-            }
-        }
+        
     }
 
     function deleteWilayah(){        
-            $sql = 'DELETE FROM t_wilayah
-            WHERE id_wilayah = :id_wilayah';
-            
-            $stmt = $pdo->prepare($sql);
-            $stmt->execute(['id_wilayah' => $_POST['id_wilayah']]);
-            header('Location: kelola_wilayah.php?status=deletesuccess');       
+                
     }
 
 
