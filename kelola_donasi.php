d1f1506076ed44f1ee93028b442d71282b6fdf0f
Feisal Abdurrahman <48938403+FeisalAR@users.noreply.github.com>
2020-11-22 15:43:59 +0700

    Update kelola_donasi.php

--- kelola_donasi.php
+++ kelola_donasi.php
@@ -242,6 +242,9 @@
     <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
     <!-- AdminLTE App -->
     <script src="dist/js/adminlte.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    
 
 </body>
 </html>
