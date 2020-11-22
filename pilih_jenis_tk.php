2a15cb36e3589254fc0c0919316b711bc4ef2887
Feisal Abdurrahman <48938403+FeisalAR@users.noreply.github.com>
2020-11-22 23:00:58 +0700

    ganti nama file jadi pilih_jenis_tk & pilih_terumbu_karang

--- pilih_jenis_tk.php
+++ pilih_jenis_tk.php
@@ -134,30 +134,7 @@
             
         
         </div>
-    </section>
-
-    <div class="album py-5 bg-light">
-        <div class="container">
-
-        <div class="row">
-            <div class="col-md-4">
-            <div class="card mb-4 shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
-                <div class="card-body">
-                <p class="card-text"><?=$rowitem->deskripsi_jenis?></p>
-                <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                </div>
-                </div>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
+    </section>    
 
     </main>
             <!-- /.row -->
