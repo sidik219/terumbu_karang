e56a04b372da639b858ccf256d0dc12cdaa637b6
Feisal Abdurrahman <48938403+FeisalAR@users.noreply.github.com>
2021-02-28 19:47:54 +0700

    hide input yg belum diperlukan

--- register.php
+++ register.php
@@ -141,7 +141,7 @@ if (isset($_POST['register'])) {
         <form action="" enctype="multipart/form-data" method="POST">
                     <div class="form-group">
                     <div class="form-group">
-                        <label for="tb_nama_user">Nama User</label>
+                        <label for="tb_nama_user">Nama Lengkap</label>
                         <input type="text" id="tb_nama_user" name="tb_nama_user" class="form-control">
                     </div>
                     <div class="form-group">
@@ -167,7 +167,7 @@ if (isset($_POST['register'])) {
                         <label for="tb_alamat_user">Alamat</label>
                         <input type="text" id="tb_email" name="tb_alamat_user" class="form-control">
                     </div>
-                    <div class="form-group">
+                    <!-- <div class="form-group">
                         <label for="num_ktp_user">No. KTP</label>
                         <input type="number" id="num_ktp_user" name="num_ktp_user" class="form-control">
                     </div>
@@ -176,23 +176,23 @@ if (isset($_POST['register'])) {
                         <div class="file-form">
                         <input type="file" id="image_uploads1" name="image_uploads1" class="form-control">
                         </div>
-                    </div>
-                    <div class="form-group">
+                    </div> -->
+                    <!-- <div class="form-group">
                         <label for="tb_tempat_lahir">Tempat Lahir</label>
                         <input type="text" id="tb_tempat_lahir" name="tb_tempat_lahir" class="form-control">
-                    </div>
+                    </div> -->
                     <div class="form-group">
                          <label for="date_tanggal_lahir">Tanggal Lahir</label>
                          <div class="file-form">
                          <input type="date" id="date_tanggal_lahir" name="date_tanggal_lahir" class="form-control" >
                          </div>
                      </div>
-                     <div class="form-group">
+                     <!-- <div class="form-group">
                         <label for="image_uploads2">Foto Diri</label>
                         <div class="file-form">
                         <input type="file" id="image_uploads2" name="image_uploads2" class="form-control">
                         </div>
-                    </div>
+                    </div> -->
                     <div class="form-group">
                         <label for="tb_username">Username</label>
                         <input type="text" id="tb_username" name="tb_username" class="form-control">
@@ -203,7 +203,7 @@ if (isset($_POST['register'])) {
                     </div>
                     <br>
                     <p align="center">
-                         <button type="submit" name="register" class="btn btn-submit">Kirim</button></p>
+                         <button type="submit" name="register" class="btn btn-submit">Daftar</button></p>
                     </form>
 
 </div>
