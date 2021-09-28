f79f3be9d044b984ca8808b7ef5196a6da845fc4
Nabil Fadhiilah <nabil.10518027@mahasiswa.unikom.ac.id>
2021-09-28 14:58:37 +0700

    Update input_tk.php
    limit size foto dan required input

--- input_tk.php
+++ input_tk.php
@@ -1,36 +1,35 @@
 <?php include 'build/config/connection.php';
 session_start();
-if(!($_SESSION['level_user'] == 2 || $_SESSION['level_user'] == 4)){
-  header('location: login.php?status=restrictedaccess');
+if (!($_SESSION['level_user'] == 2 || $_SESSION['level_user'] == 4)) {
+    header('location: login.php?status=restrictedaccess');
 }
 $url_sekarang = basename(__FILE__);
 include 'hak_akses.php';
 
-    $sqlviewjenis = 'SELECT * FROM t_jenis_terumbu_karang
+$sqlviewjenis = 'SELECT * FROM t_jenis_terumbu_karang
                         ORDER BY id_jenis';
-        $stmt = $pdo->prepare($sqlviewjenis);
-        $stmt->execute();
-        $row = $stmt->fetchAll();
+$stmt = $pdo->prepare($sqlviewjenis);
+$stmt->execute();
+$row = $stmt->fetchAll();
 
-    if (isset($_POST['submit'])) {
-            $id_jenis      = $_POST['dd_id_jenis'];
-            $nama_terumbu_karang        = $_POST['tb_nama_terumbu'];
-            $deskripsi_terumbu_karang        = $_POST['tb_deskripsi_terumbu'];
-            $randomstring = substr(md5(rand()), 0, 7);
-            $harga_terumbu_karang = $_POST['num_harga_terumbu_karang'];
-            $deskripsi_panjang_tk = $_POST['deskripsi_panjang_tk'];
+if (isset($_POST['submit'])) {
+    $id_jenis      = $_POST['dd_id_jenis'];
+    $nama_terumbu_karang        = $_POST['tb_nama_terumbu'];
+    $deskripsi_terumbu_karang        = $_POST['tb_deskripsi_terumbu'];
+    $randomstring = substr(md5(rand()), 0, 7);
+    $harga_terumbu_karang = $_POST['num_harga_terumbu_karang'];
+    $deskripsi_panjang_tk = $_POST['deskripsi_panjang_tk'];
 
-            //Image upload
-        if($_FILES["image_uploads"]["size"] == 0) {
-            $foto_terumbu_karang = "images/image_default.jpg";
-        }
-        else if (isset($_FILES['image_uploads'])) {
-            $target_dir  = "images/foto_terumbu_karang/";
-            $foto_terumbu_karang = $target_dir .'TK_'.$randomstring. '.jpg';
-            move_uploaded_file($_FILES["image_uploads"]["tmp_name"], $foto_terumbu_karang);
-        }
+    //Image upload
+    if ($_FILES["image_uploads"]["size"] == 0) {
+        $foto_terumbu_karang = "images/image_default.jpg";
+    } else if (isset($_FILES['image_uploads'])) {
+        $target_dir  = "images/foto_terumbu_karang/";
+        $foto_terumbu_karang = $target_dir . 'TK_' . $randomstring . '.jpg';
+        move_uploaded_file($_FILES["image_uploads"]["tmp_name"], $foto_terumbu_karang);
+    }
 
-        //---image upload end
+    //---image upload end
 
     $sqlterumbu_karang = "INSERT INTO t_terumbu_karang
                             (id_jenis, nama_terumbu_karang, deskripsi_terumbu_karang, foto_terumbu_karang, harga_terumbu_karang, deskripsi_panjang_tk)
@@ -38,33 +37,36 @@ include 'hak_akses.php';
                             :deskripsi_terumbu_karang, :foto_terumbu_karang, :harga_terumbu_karang, :deskripsi_panjang_tk)";
 
     $stmt = $pdo->prepare($sqlterumbu_karang);
-    $stmt->execute(['id_jenis' => $id_jenis,'nama_terumbu_karang' => $nama_terumbu_karang,
-    'deskripsi_terumbu_karang' => $deskripsi_terumbu_karang,
-    'foto_terumbu_karang' => $foto_terumbu_karang, 'harga_terumbu_karang' => $harga_terumbu_karang, 'deskripsi_panjang_tk' => $deskripsi_panjang_tk]);
+    $stmt->execute([
+        'id_jenis' => $id_jenis, 'nama_terumbu_karang' => $nama_terumbu_karang,
+        'deskripsi_terumbu_karang' => $deskripsi_terumbu_karang,
+        'foto_terumbu_karang' => $foto_terumbu_karang, 'harga_terumbu_karang' => $harga_terumbu_karang, 'deskripsi_panjang_tk' => $deskripsi_panjang_tk
+    ]);
 
     $affectedrows = $stmt->rowCount();
     if ($affectedrows == '0') {
-    //echo "HAHAHAAHA INSERT FAILED !";
+        //echo "HAHAHAAHA INSERT FAILED !";
     } else {
         //echo "HAHAHAAHA GREAT SUCCESSS !";
         header("Location: kelola_tk.php?status=addsuccess");
-        }
     }
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Kelola Terumbu Karang - GoKarang</title>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Font Awesome -->
-        <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
     <!-- Theme style -->
-        <link rel="stylesheet" href="dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="dist/css/adminlte.min.css">
     <!-- overlayScrollbars -->
-        <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="js/trumbowyg/dist/ui/trumbowyg.min.css">
     <script src="js/trumbowyg/dist/trumbowyg.js"></script>
     <!-- Local CSS -->
@@ -88,9 +90,9 @@ include 'hak_akses.php';
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Akun Saya</a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <a class="dropdown-item" href="#">Edit Profil</a>
-                            <a class="dropdown-item" href="logout.php">Logout</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="#">Edit Profil</a>
+                        <a class="dropdown-item" href="logout.php">Logout</a>
                 </li>
             </ul>
         </nav>
@@ -108,8 +110,9 @@ include 'hak_akses.php';
             <div class="sidebar">
                 <!-- SIDEBAR MENU -->
                 <nav class="mt-2">
-                   <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                    <?php print_sidebar(basename(__FILE__), $_SESSION['level_user'])?> <!-- Print sidebar -->
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                        <?php print_sidebar(basename(__FILE__), $_SESSION['level_user']) ?>
+                        <!-- Print sidebar -->
                     </ul>
                 </nav>
                 <!-- END OF SIDEBAR MENU -->
@@ -121,10 +124,11 @@ include 'hak_akses.php';
         <div class="content-wrapper">
             <!-- Content Header (Page header) -->
             <div class="content-header">
-                    <div class="container-fluid">
-                        <a class="btn btn-outline-primary" href="kelola_tk.php">< Kembali</a><br><br>
-                        <h4><span class="align-middle font-weight-bold">Input Data Terumbu Karang</h4></span>
-                    </div>
+                <div class="container-fluid">
+                    <a class="btn btn-outline-primary" href="kelola_tk.php">
+                        < Kembali</a><br><br>
+                            <h4><span class="align-middle font-weight-bold">Input Data Terumbu Karang</h4></span>
+                </div>
                 <!-- /.container-fluid -->
             </div>
             <!-- /.content-header -->
@@ -133,83 +137,88 @@ include 'hak_akses.php';
             <section class="content">
                 <div class="container-fluid">
                     <form action="" enctype="multipart/form-data" method="POST">
-                    <div class="form-group">
-                        <label for="dd_id_jenis">ID Jenis</label>
-                        <select id="dd_id_jenis" name="dd_id_jenis" class="form-control">
-                            <?php foreach ($row as $rowitem) {
-                            ?>
-                            <option value="<?=$rowitem->id_jenis?>">ID <?=$rowitem->id_jenis?> - <?=$rowitem->nama_jenis?></option>
+                        <div class="form-group">
+                            <label for="dd_id_jenis">ID Jenis</label>
+                            <select id="dd_id_jenis" name="dd_id_jenis" class="form-control">
+                                <?php foreach ($row as $rowitem) {
+                                ?>
+                                    <option value="<?= $rowitem->id_jenis ?>">ID <?= $rowitem->id_jenis ?> - <?= $rowitem->nama_jenis ?></option>
 
-                            <?php } ?>
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label for="tb_nama_terumbu">Nama Terumbu Karang</label>
-                        <input type="text" id="tb_nama_terumbu" name="tb_nama_terumbu" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <label for="tb_deskripsi_terumbu">Deskripsi Terumbu Karang</label>
-                        <input type="text" id="tb_deskripsi_terumbu" name="tb_deskripsi_terumbu" class="form-control">
-                    </div>
-
-                    <div class="form-group">
-                        <label for="isi_artikel">Deskripsi Lengkap Terumbu Karang</label>
-                        <textarea id="deskripsi_lengkap_tk" name="deskripsi_panjang_tk"></textarea>
-                    <script>
-                            $('#deskripsi_lengkap_tk').trumbowyg();
-                    </script>
-                    </div>
-
-                    <div class='form-group' id='fototk'>
-                        <div>
-                            <label for='image_uploads'>Upload Foto Terumbu Karang</label>
-                            <input type='file'  class='form-control' id='image_uploads'
-                                name='image_uploads' accept='.jpg, .jpeg, .png' onchange="readURL(this);">
+                                <?php } ?>
+                            </select>
                         </div>
-                    </div>
-                    <div class="form-group">
-                        <img id="preview"  width="100px" src="#" alt="Preview Gambar"/>
+                        <div class="form-group">
+                            <label for="tb_nama_terumbu">Nama Terumbu Karang</label>
+                            <input type="text" id="tb_nama_terumbu" name="tb_nama_terumbu" class="form-control" required>
+                        </div>
+                        <div class="form-group">
+                            <label for="tb_deskripsi_terumbu">Deskripsi Terumbu Karang</label>
+                            <input type="text" id="tb_deskripsi_terumbu" name="tb_deskripsi_terumbu" class="form-control" required>
+                        </div>
 
-                        <script>
-                            window.onload = function() {
-                            document.getElementById('preview').style.display = 'none';
-                            };
-                            function readURL(input) {
-                                if (input.files && input.files[0]) {
-                                    var reader = new FileReader();
+                        <div class="form-group">
+                            <label for="isi_artikel">Deskripsi Lengkap Terumbu Karang</label>
+                            <textarea id="deskripsi_lengkap_tk" name="deskripsi_panjang_tk" required></textarea>
+                            <script>
+                                $('#deskripsi_lengkap_tk').trumbowyg();
+                            </script>
+                        </div>
 
-                                    reader.onload = function (e) {
-                                        $('#preview')
-                                            .attr('src', e.target.result)
-                                            .width(200);
-                                            document.getElementById('preview').style.display = 'block';
+                        <div class='form-group' id='fototk'>
+                            <div>
+                                <label for='image_uploads'>Upload Foto Terumbu Karang</label>
+                                <input type='file' class='form-control' id='image_uploads' name='image_uploads' accept='.jpg, .jpeg, .png' onchange="readURL(this);" required>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <img id="preview" width="100px" src="#" alt="Preview Gambar" />
+
+                            <script>
+                                window.onload = function() {
+                                    document.getElementById('preview').style.display = 'none';
+                                };
+
+                                function readURL(input) {
+                                    if (input.files[0].size > 2000000) { // ini untuk ukuran 800KB, 2000000 untuk 2MB.
+                                        alert("Maaf, Ukuran File Terlalu Besar. !Maksimal Upload 2MB");
+                                        input.value = "";
                                     };
+                                    if (input.files && input.files[0]) {
+                                        var reader = new FileReader();
 
-                                    reader.readAsDataURL(input.files[0]);
+                                        reader.onload = function(e) {
+                                            $('#preview')
+                                                .attr('src', e.target.result)
+                                                .width(200);
+                                            document.getElementById('preview').style.display = 'block';
+                                        };
+
+                                        reader.readAsDataURL(input.files[0]);
+                                    }
                                 }
-                            }
-                        </script>
-                    </div>
-                    <div class="form-group d-none">
-                        <label for="num_harga_terumbu_karang">Harga Terumbu Karang</label>
-                        <input type="number" id="num_harga_terumbu_karang" name="num_harga_terumbu_karang" class="form-control" value="1">
-                    </div>
+                            </script>
+                        </div>
+                        <div class="form-group d-none">
+                            <label for="num_harga_terumbu_karang">Harga Terumbu Karang</label>
+                            <input type="number" id="num_harga_terumbu_karang" name="num_harga_terumbu_karang" class="form-control" value="1">
+                        </div>
 
-                    <br>
-                    <p align="center">
-                            <button type="submit" name="submit" value="Simpan" class="btn btn-submit">Simpan</button></p>
+                        <br>
+                        <p align="center">
+                            <button type="submit" name="submit" value="Simpan" class="btn btn-submit">Simpan</button>
+                        </p>
                     </form>
-            <br><br>
+                    <br><br>
 
             </section>
 
             <!-- /.Left col -->
-            </div>
-            <!-- /.row (main row) -->
         </div>
-        <!-- /.container-fluid -->
-        </section>
-        <!-- /.content -->
+        <!-- /.row (main row) -->
+    </div>
+    <!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
     <br><br>
@@ -219,7 +228,7 @@ include 'hak_akses.php';
 
     <!-- Control Sidebar -->
     <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
+        <!-- Control sidebar content goes here -->
     </aside>
     <!-- /.control-sidebar -->
     </div>
@@ -234,7 +243,8 @@ include 'hak_akses.php';
     <!-- AdminLTE App -->
     <script src="dist/js/adminlte.js"></script>
     <!-- Import Trumbowyg font size JS at the end of <body>... -->
-<script src="js/trumbowyg/dist/plugins/fontsize/trumbowyg.fontsize.min.js"></script>
+    <script src="js/trumbowyg/dist/plugins/fontsize/trumbowyg.fontsize.min.js"></script>
 
 </body>
+
 </html>
